426ec89890d704fd89bba77633e23b2a3812825b
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-20 14:54:20 +0900

    crack

---
+++ dictionary-attack/src/lib.rs
@@ -0,0 +1,56 @@
+/// Generates a string Vec. For use with tests.
+pub fn generate_string(i: u64) -> Vec<u8> {
+    let mut array = [0u8; 20];
+    generate_char_array(i, &mut array).to_vec()
+}
+
+
+/// Generates a string slice (in u8) to crack based on an index seed.
+/// An array needs to be passed to avoid performance penalties with allocating a Vec.
+fn generate_char_array(mut i: u64, reversed: &mut [u8; 20]) -> &[u8] {
+    if i == 0 {
+        return &[];
+    }
+    let radix = 26;
+    const A_DEC: u8 = 97;
+
+    let mut digit = 0;
+    while i > 0 {
+        let remainder = i % radix;
+        let remainder_zero_shifted = if remainder == 0 { radix } else { remainder };
+        let m = remainder_zero_shifted - 1;
+
+        i = (i - remainder_zero_shifted) / radix;
+
+        reversed[digit] = A_DEC + m as u8;
+
+        digit += 1;
+    }
+    for i in 0..(digit / 2) {
+        let swap = reversed[digit - i - 1];
+        reversed[digit - i - 1] = reversed[i];
+        reversed[i] = swap;
+    }
+    &(reversed[0..digit])
+}
+
+#[test]
+fn test_calculate_str_len() {
+    assert_eq!(0, generate_string(0).len());
+    assert_eq!(1, generate_string(1).len());
+    assert_eq!(1, generate_string(26).len());
+    assert_eq!(2, generate_string(27).len());
+    assert_eq!(2, generate_string(702).len());
+    assert_eq!(3, generate_string(703).len());
+}
+
+
+#[test]
+fn test_generate_string() {
+    assert_eq!("", String::from_utf8(generate_string(0)).unwrap());
+    assert_eq!("a", String::from_utf8(generate_string(1)).unwrap());
+    assert_eq!("z", String::from_utf8(generate_string(26)).unwrap());
+    assert_eq!("aa", String::from_utf8(generate_string(27)).unwrap());
+    assert_eq!("zz", String::from_utf8(generate_string(702)).unwrap());
+    assert_eq!("aaa", String::from_utf8(generate_string(703)).unwrap());
+}
--- dictionary-attack/src/main.rs
+++ dictionary-attack/src/main.rs
@@ -1,31 +1,45 @@
 use std::io;
 
 fn validate_password(pass: &String, _max_password_length: usize) -> Result<String, ()> {
-    let result: bool = !pass.trim().is_empty() && pass.trim().len() < _max_password_length+1;
+    let result: bool = !pass.trim().is_empty() && pass.trim().len() < _max_password_length + 1;
 
     println!("{}", pass.len());
 
     if result {
-        let new_pass = pass.clone();
-        Ok(new_pass)
+        let new_pass = pass.trim().clone();
+        Ok(String::from(new_pass))
     } else {
-        Err({
-            println!("password should be 1 ~ {} words.", _max_password_length)
-        })
+        Err(println!(
+            "password should be 1 ~ {} words.",
+            _max_password_length
+        ))
     }
 }
 
+mod lib;
+
 fn main() {
-    const MAX_PASSWORD_LENGTH: usize = 4;
+    const MAX_PASSWORD_LENGTH: usize = 6;
 
     println!("Please input password.");
     let mut password = String::new();
-
     io::stdin()
         .read_line(&mut password)
         .expect("Failed to read line");
 
+    // let password_bytes = Vec::from(password);
     let result = validate_password(&password, MAX_PASSWORD_LENGTH).unwrap();
 
     println!("password: {}", result);
+    println!("cracking ...");
+
+    for i in 1.. {
+        if result == String::from_utf8(lib::generate_string(i)).unwrap() {
+            println!("no.{}", i);
+            println!("found {}", result);
+            break;
+        } else {
+            continue;
+        }
+    }
 }
