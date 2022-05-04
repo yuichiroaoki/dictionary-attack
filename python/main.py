3c824c13ea0804f1869a44f69b35e7379397bd79
TUXedoJP <91130562+TUXedoJP@users.noreply.github.com>
2022-05-04 11:12:35 +0900

    Add files via upload

--- python/main.py
+++ python/main.py
@@ -1,22 +1,30 @@
+# xato-net-10-million-passwords.txtから指定のパスワード探す
+
 import glob
 import time
+from pathlib import Path
 
-password = input(“パスワードを入力してください:“)
-# 計測開始
-start_time = time.process_time()
+password = input("Please input password.")
+print(f'password: {password}')
+
+# 計測開始(ナノ秒)
+start_time = time.perf_counter_ns()
 # 指定ディレクトリ内からファイル名を取得
-files = glob.glob(“/Users/b/go/src/SecLists/Passwords/WiFi-WPA/*“)
+files = glob.glob("/Users/b/go/src/SecLists/Passwords/xato-net-10-million-passwords.txt")
+# ファイルを一つずつ処理
 for file in files:
-    #with open(‘/Users/b/go/src/SecLists/Passwords/xato-net-10-million-passwords.txt’) as f:
-    # ファイルを一つずつ処理
-    with open(file) as f:
-        # パスワードリストから１行ずつ取得してチェック
-        for line in f:
-            if line.strip() == password:
-                print(f’found {line.strip()}’)
-                # 計測終了
-                end_time = time.process_time()
-                # 経過時間を出力(秒)
-                elapsed_time = end_time - start_time
-                print(f”{elapsed_time}“)
-                continue
+    try:
+        with open(file) as f:
+            # パスワードリストから１行ずつ取得してチェック
+            for line in f:
+                if line.strip() == password:
+                    # 計測終了(ナノ秒)
+                    end_time = time.perf_counter_ns()
+                    print(f'✔ cracking')
+                    # 経過時間を出力(ミリ秒)
+                    elapsed_time = (end_time - start_time) / 1000000
+                    print(f"{elapsed_time}ms")
+                    continue
+    # 'utf-8' codec can't decode
+    except:
+        print(f"error")
