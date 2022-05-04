f3f330aa46879c8c0ae5b6597f6c95393ec066c3
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-05-04 11:12:10 +0900

    add comments

--- dictionary-attack/src/main.rs
+++ dictionary-attack/src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::fs::File;
-use std::io;
+// use std::io;
 use std::io::prelude::*;
 use std::time::Instant;
 use terminal_spinners::{SpinnerBuilder, DOTS};
@@ -8,10 +8,14 @@ use terminal_spinners::{SpinnerBuilder, DOTS};
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// dictionary attack with wordlists
     #[clap(short)]
     dict: bool,
-    #[clap(short, long)]
-    name: String,
+    /// performance test mode
+    #[clap(short)]
+    performance: bool,
+
+    /// password for wifi
     #[clap(short, long)]
     password: String,
 }
@@ -25,14 +29,16 @@ fn main() {
     lib::number_to_string(36);
     const MAX_PASSWORD_LENGTH: usize = 10;
     // wifi::get_wifi_name();
-    wifi::connect_to_wifi(&args.name, &args.password);
+    // wifi::connect_to_wifi(&args.name, &args.password);
+
     println!("Please input password.");
-    let mut password = String::new();
-    io::stdin()
-        .read_line(&mut password)
-        .expect("Failed to read line");
+    let password = args.password;
+    // let mut password = String::new();
+    // io::stdin()
+    //     .read_line(&mut password)
+    //     .expect("Failed to read line");
     let result = validate::validate_password(&password, MAX_PASSWORD_LENGTH).unwrap();
-    println!("password: {}", result);
+
     let now = Instant::now();
 
     let handle = SpinnerBuilder::new()
