ca11e4e0ac09e3bbe77b0501c86632a5ecc8662a
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-05-04 16:57:35 +0900

    new line

--- dictionary-attack/src/main.rs
+++ dictionary-attack/src/main.rs
@@ -2,9 +2,9 @@ use clap::Parser;
 use std::fs::File;
 // use std::io;
 use std::io::prelude::*;
+use std::thread;
 use std::time::Instant;
 use terminal_spinners::{SpinnerBuilder, DOTS};
-use std::thread;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -48,28 +48,25 @@ fn main() {
         .start();
 
     if args.dict {
-        let mut f1  = File::open("sample/sample1.txt").expect("file not found");
-        let mut f2  = File::open("sample/sample2.txt").expect("file not found");
+        let mut f1 = File::open("sample/sample1.txt").expect("file not found");
+        let mut f2 = File::open("sample/sample2.txt").expect("file not found");
         //let mut f = File::open("sample/xato-net-10-million-passwords.txt").expect("file not found");
 
         let mut contents1 = String::new();
         f1.read_to_string(&mut contents1)
             // ファイルの読み込み中に問題がありました
             .expect("something went wrong reading the file");
-        
         let mut contents2 = String::new();
         f2.read_to_string(&mut contents2)
-                // ファイルの読み込み中に問題がありました
-                .expect("something went wrong reading the file");
-        
+            // ファイルの読み込み中に問題がありました
+            .expect("something went wrong reading the file");
         let v_contents1: Vec<&str> = contents1.split('\n').collect();
         let v_contents2: Vec<&str> = contents2.split('\n').collect();
 
         let handle = thread::spawn(|| {
-
             for i in 0..v_contents1.len() {
                 if result == v_contents1[i] {
-                    println!("no.{}", i);
+                    println!("\nno.{}", i);
                     println!("found {}", result);
                     break;
                 } else {
@@ -81,19 +78,18 @@ fn main() {
 
         for i in 0..v_contents2.len() {
             if result == v_contents2[i] {
-                println!("no.{}", i);
+                println!("\nno.{}", i);
                 println!("found {}", result);
                 break;
             } else {
                 continue;
             }
         }
-        
         handle.join().unwrap();
     } else {
         for i in 1..10000000000000 {
             if result == lib::number_to_string(i) {
-                println!("no.{}", i);
+                println!("\nno.{}", i);
                 println!("found {}", result);
                 break;
             } else {
