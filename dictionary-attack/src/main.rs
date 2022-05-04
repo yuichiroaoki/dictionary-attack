e9b2356699fb5283524f26a1ca804bdcb5facf9a
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-05-04 16:15:39 +0900

    add

--- dictionary-attack/src/main.rs
+++ dictionary-attack/src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use std::fs::File;
 use std::io::prelude::*;
+use std::thread;
 use std::time::Instant;
 use terminal_spinners::{SpinnerBuilder, DOTS};
-use std::thread;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -20,6 +20,7 @@ struct Args {
     password: String,
 }
 
+mod files;
 mod lib;
 mod validate;
 mod wifi;
@@ -42,48 +43,34 @@ fn main() {
         .start();
 
     if args.dict {
-        let mut f1  = File::open("sample/sample1.txt").expect("file not found");
-        let mut f2  = File::open("sample/sample2.txt").expect("file not found");
-        //let mut f = File::open("sample/xato-net-10-million-passwords.txt").expect("file not found");
+        let result_cp = result.clone();
+        if let Ok(lines) = files::read_lines("sample/sample1.txt") {
+            thread::spawn(move || {
+                for line in lines {
+                    if let Ok(ip) = line {
+                        if result == ip {
+                            println!("found {}", result);
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+            });
+        }
 
-        let mut contents1 = String::new();
-        f1.read_to_string(&mut contents1)
-            // ファイルの読み込み中に問題がありました
-            .expect("something went wrong reading the file");
-        
-        let mut contents2 = String::new();
-        f2.read_to_string(&mut contents2)
-                // ファイルの読み込み中に問題がありました
-                .expect("something went wrong reading the file");
-        
-        let v_contents1: Vec<&str> = contents1.split('\n').collect();
-        let v_contents2: Vec<&str> = contents2.split('\n').collect();
-
-        let handle = thread::spawn(|| {
-
-            for i in 0..v_contents1.len() {
-                if result == v_contents1[i] {
-                    println!("no.{}", i);
-                    println!("found {}", result);
-                    break;
-                } else {
-                    continue;
+        if let Ok(lines) = files::read_lines("sample/sample2.txt") {
+            for line in lines {
+                if let Ok(ip) = line {
+                    if result_cp == ip {
+                        println!("found {}", result_cp);
+                        break;
+                    } else {
+                        continue;
+                    }
                 }
             }
-            // thread code
-        });
-
-        for i in 0..v_contents2.len() {
-            if result == v_contents2[i] {
-                println!("no.{}", i);
-                println!("found {}", result);
-                break;
-            } else {
-                continue;
-            }
         }
-        
-        handle.join().unwrap();
     } else {
         for i in 1..10000000000000 {
             if result == lib::number_to_string(i) {
