8fdd86f32e1b999e7c499f326637f8ae659c66cc
TUXedoJP <91130562+TUXedoJP@users.noreply.github.com>
2022-05-04 11:49:59 +0900

    Add files via upload

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-# xato-net-10-million-passwords.txtから指定のパスワード探す
+ # xato-net-10-million-passwords.txtから指定のパスワード探す
 
 import glob
 import time
@@ -9,8 +9,8 @@ print(f'password: {password}')
 
 # 計測開始(ナノ秒)
 start_time = time.perf_counter_ns()
-# 指定ディレクトリ内からファイル名を取得
-files = glob.glob("/Users/b/go/src/SecLists/Passwords/xato-net-10-million-passwords.txt")
+# 指定ディレクトリ内からファイル名を取得 
+files = glob.glob("./xato-net-10-million-passwords.txt")
 # ファイルを一つずつ処理
 for file in files:
     try:
