591e8d65660ad7ca01f73860ef464fd03bac6d8b
yuichiroaoki <yuichiroaoki27@gmail.com>
2022-05-11 14:16:51 +0900

    comment out

--- dictionary-attack/src/wifi.rs
+++ dictionary-attack/src/wifi.rs
@@ -1,33 +1,33 @@
-use std::error::Error;
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+// use std::error::Error;
+// use std::io::{BufRead, BufReader};
+// use std::process::{Command, Stdio};
 // use clap::lazy_static::lazy_static;
 use crate::utils;
 use wifi_rs::{prelude::*, WiFi};
 // use regex::Regex;
 
-pub fn get_wifi_name() -> Result<(), Box<dyn Error>> {
-    let stdout = Command::new("iwlist")
-        .arg("wlo1")
-        .arg("scan")
-        .stdout(Stdio::piped())
-        .spawn()?
-        .stdout
-        .expect("failed to execute process");
+// pub fn get_wifi_name() -> Result<(), Box<dyn Error>> {
+//     let stdout = Command::new("iwlist")
+//         .arg("wlo1")
+//         .arg("scan")
+//         .stdout(Stdio::piped())
+//         .spawn()?
+//         .stdout
+//         .expect("failed to execute process");
 
-    let reader = BufReader::new(stdout);
+//     let reader = BufReader::new(stdout);
 
-    let target_line = reader
-        .lines()
-        .filter_map(|line| line.ok())
-        .filter(|line| line.find("ESSID").is_some())
-        .next();
-    // .for_each(|line| println!("{}", line));
+//     let target_line = reader
+//         .lines()
+//         .filter_map(|line| line.ok())
+//         .filter(|line| line.find("ESSID").is_some())
+//         .next();
+//     // .for_each(|line| println!("{}", line));
 
-    println!("{:?}", target_line);
+//     println!("{:?}", target_line);
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 // pub fn example_command() {
 // 	let output = Command::new("ls")
@@ -39,21 +39,21 @@ pub fn get_wifi_name() -> Result<(), Box<dyn Error>> {
 // 	println!("{}", stdout);
 // }
 
-pub fn connect_to_wifi_with_command(name: &str, password: &str) {
-    let output = Command::new("nmcli")
-        .arg("d")
-        .arg("wifi")
-        .arg("connect")
-        .arg(name)
-        .arg("password")
-        .arg(password)
-        .stdout(Stdio::piped())
-        .output()
-        .expect("failed to execute process");
-    let stdout = String::from_utf8(output.stdout).unwrap();
+// pub fn connect_to_wifi_with_command(name: &str, password: &str) {
+//     let output = Command::new("nmcli")
+//         .arg("d")
+//         .arg("wifi")
+//         .arg("connect")
+//         .arg(name)
+//         .arg("password")
+//         .arg(password)
+//         .stdout(Stdio::piped())
+//         .output()
+//         .expect("failed to execute process");
+//     let stdout = String::from_utf8(output.stdout).unwrap();
 
-    println!("{}", stdout);
-}
+//     println!("{}", stdout);
+// }
 
 pub fn connect_to_wifi(password: &str) -> Result<(), WifiConnectionError> {
     let config = Some(Config {
